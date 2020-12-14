516478c2e032d255d8cfcff92078b1a6f0e5e9d6
Padraig  Gleeson <p.gleeson@gmail.com>
2020-12-14 12:54:19 +0000

    Regenerated & tested

---
+++ TVB_LEMS/test_all.sh
@@ -0,0 +1,11 @@
+set -e
+
+jnml LEMS_TestRWW.xml -nogui
+jnml LEMS_TestLinear.xml -nogui
+jnml LEMS_TestKuramoto.xml -nogui
+jnml LEMS_TestJR.xml -nogui
+jnml LEMS_TestG2DO_2.xml -nogui
+jnml LEMS_TestG2DO.xml -nogui
+
+
+
