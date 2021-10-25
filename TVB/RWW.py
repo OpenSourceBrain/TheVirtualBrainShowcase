29a617342eb475615a7dfb795fe0c6639285335e
Padraig Gleeson <p.gleeson@gmail.com>
2021-10-25 12:18:37 +0100

    Test tvb example

--- TVB/RWW.py
+++ TVB/RWW.py
@@ -1,16 +1,23 @@
-from tvb.simulator.lab import *
+from tvb.simulator.lab import models
 import numpy as np
 from matplotlib import pyplot as plt
 
+print('Creating RWW')
 
 rww = models.ReducedWongWang(a=0.27, w=1.0, I_o=0.3)
+print('RWW: %s'%rww)
+
+
 S = np.linspace(0, 1, 50).reshape((1, -1, 1))
 C = S * 0.0
 dS = rww.dfun(S, C)
 
-plt.figure()
-plt.plot(S.flat, dS.flat)
+import sys
+if not '-nogui' in sys.argv:
 
-plt.show()
+    plt.figure()
+    plt.plot(S.flat, dS.flat)
 
+    plt.show()
 
+print('Done!')
