efc923777c116696c366c93582253546686ae97f
Padraig Gleeson <p.gleeson@gmail.com>
2022-05-11 11:09:34 +0100

    Test lems

--- TVB_LEMS/test_all.sh
+++ TVB_LEMS/test_all.sh
@@ -1,11 +1,8 @@
 set -e
 
-jnml LEMS_TestRWW.xml -nogui
-jnml LEMS_TestLinear.xml -nogui
-jnml LEMS_TestKuramoto.xml -nogui
-jnml LEMS_TestJR.xml -nogui
-jnml LEMS_TestG2DO_2.xml -nogui
-jnml LEMS_TestG2DO.xml -nogui
-
-
-
+pynml LEMS_TestRWW.xml -nogui
+pynml LEMS_TestLinear.xml -nogui
+pynml LEMS_TestKuramoto.xml -nogui
+pynml LEMS_TestJR.xml -nogui
+pynml LEMS_TestG2DO_2.xml -nogui
+pynml LEMS_TestG2DO.xml -nogui
